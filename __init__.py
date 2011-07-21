b2a04e3a1aa71fc835d3739ffa3583cc9edfc6b0
Matthew Bell <matthewrobertbell@gmail.com>
2011-07-21 17:44:56 +0100

    initial working backlinking pulling

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
 from ahrefs import *
+
+if __name__ == '__main__':
+	obj = ahrefs('fakekey')
+	print obj.links('http://www.reddit.com/')
--- ahrefs.py
+++ ahrefs.py
@@ -1,22 +1,55 @@
 import urllib2
+import datetime
 from collections import defaultdict
+from lxml import etree
 
 class ahrefs(object):
 	def __init__(self,key):
 		self.key = key
+		self.namespace = 'http://ahrefs.com/schemas/api/links/1'
 		
-	def links(self,target,mode=exact,internal=False,count=1000,timeout=30,filter_nofollow=None,filter_link_type=None):
-		request_url = 'http://ahrefs.com/api.php?AhrefsKey=%s&type=inlinks&mode=%s&include_internal=%s&count=1000&target=%s' % (self.key,mode,str(internal).lower(),count,target)
+	def links(self,target,mode='exact',internal=False,count=1000,timeout=30,filter_nofollow=None,filter_link_type=None,filter_date_newer=None,filter_date_older=None):
+		request_url = 'http://ahrefs.com/api.php?AhrefsKey=%s&type=inlinks&mode=%s&include_internal=%s&count=%s&target=%s' % (self.key,mode,str(internal).lower(),count,target)
+		print request_url
+		#response = open('example.xml').read()
 		response = urllib2.urlopen(request_url,timeout=timeout).read()
 		response_doc = etree.XML(response)
-		results = {}
+		results = defaultdict(list)
 		
-		for result in []:
-			parsed_result = defaultdict(list)
-			destination_url = result.xpath('destination_url').text()
-
+		for result in response_doc.xpath('//n:result',namespaces={'n': self.namespace}):
+			parsed_result = {}
+			for child in result.iterchildren():
+				text = child.text.strip()
+				tag = child.tag.strip().replace('{'+self.namespace+'}','')
+				if text.lower() == 'false':
+					text = False
+				else:
+					if text.lower() == 'true':
+						text = True
+				if tag == 'visited':
+					text = datetime.datetime.strptime(text,'%Y-%m-%dT%H:%M:%SZ')
+				parsed_result[tag] = text
 			
-			visited = datetime.datetime.strptime(result.xpath('visited').text(),'%Y-%m-%dT%H:%M:%SZ')
-			
-			results[destination_url].append(parsed_result)
+			if filter_link_type:
+				if parsed_result['link_type'] not in filter_link_type:
+					break
+			if filter_nofollow != None:
+				if filter_nofollow:
+					if not parsed_result['is_nofollow']:
+						break
+				else:
+					if parsed_result['is_nofollow']:
+						break
+			print parsed_result
+			if filter_date_newer:
+				if isinstance(filter_date_newer,int):
+					filter_date = datetime.timedelta(days=filter_date)
+				if datetime.date.today() - filter_date_newer > parsed_result['visisted']:
+					break
+			if filter_date_older:
+				if isinstance(filter_date_older,int):
+					filter_date = datetime.timedelta(days=filter_date)
+				if datetime.date.today() - filter_date_older < parsed_result['visisted']:
+					break
+			results[parsed_result['destination_url']].append(parsed_result)
 		return results	
