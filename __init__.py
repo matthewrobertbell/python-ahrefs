637a6ecacf2dc9845124be4d613029770878a010
Matthew Bell <matthewrobertbell@gmail.com>
2011-07-22 13:13:59 +0100

    pages working, cast numbers to int/float, change xml schema depending on methodd

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from ahrefs import *
 
 if __name__ == '__main__':
 	obj = ahrefs('fakekey')
-	print obj.links('http://www.reddit.com/')
+	print obj.pages('http://www.reddit.com/')
--- ahrefs.py
+++ ahrefs.py
@@ -6,38 +6,41 @@ from lxml import etree
 class ahrefs(object):
 	def __init__(self,key):
 		self.key = key
-		self.namespace = 'http://ahrefs.com/schemas/api/links/1'
+		self.namespace = 'http://ahrefs.com/schemas/api/%s/1'
 		
 	def request(self,request_url,timeout):
 		response = urllib2.urlopen(request_url,timeout=timeout).read()
+		print response
 		return etree.XML(response)
 		
-	def parse_result(self,result):
+	def parse_result(self,result,method_name):
 		parsed_result = {}
 		for child in result.iterchildren():
 			text = child.text.strip()
-			tag = child.tag.strip().replace('{'+self.namespace+'}','')
+			tag = child.tag.strip().replace('{'+self.namespace % method_name+'}','')
 			if text.lower() in ('true','false'):
 				text = text.lower() == 'true'
 			if tag == 'visited':
 				text = datetime.datetime.strptime(text,'%Y-%m-%dT%H:%M:%SZ')
-			parsed_result[tag] = text			parsed_result = {}
+			parsed_result[tag] = text
 		for child in result.iterchildren():
 			text = child.text.strip()
-			tag = child.tag.strip().replace('{'+self.namespace+'}','')
+			tag = child.tag.strip().replace('{'+self.namespace % method_name+'}','')
 			if text.lower() in ('true','false'):
 				text = text.lower() == 'true'
 			if tag == 'visited':
 				text = datetime.datetime.strptime(text,'%Y-%m-%dT%H:%M:%SZ')
-			if '.' in text:
-				try:
-					text = float(text)
-				except:
-					pass
-			try:
-				text = int(text)
-			except:
-				pass
+			if isinstance(text,basestring):
+				if '.' in text:
+					try:
+						text = float(text)
+					except:
+						pass
+				else:
+					try:
+						text = int(text)
+					except:
+						pass
 			parsed_result[tag] = text
 		return parsed_result
 		
@@ -56,11 +59,12 @@ class ahrefs(object):
 		
 		
 	def links(self,target,mode='exact',internal=False,count=1000,timeout=30,filter_nofollow=None,filter_link_type=None,filter_date_newer=None,filter_date_older=None):
+		mathod_name = 'links'
 		request_url = 'http://ahrefs.com/api.php?AhrefsKey=%s&type=inlinks&mode=%s&include_internal=%s&count=%s&target=%s' % (self.key,mode,str(internal).lower(),count,target)
 		response = self.request(request_url,timeout)
 		results = defaultdict(list)
-		for result in response.xpath('//n:result',namespaces={'n': self.namespace}):
-			parsed_result = self.parse_result(result)
+		for result in response.xpath('//n:result',namespaces={'n': self.namespace % method_name}):
+			parsed_result = self.parse_result(result,method_name)
 			if filter_link_type:
 				if parsed_result['link_type'] not in filter_link_type:
 					break
@@ -76,16 +80,20 @@ class ahrefs(object):
 			results[parsed_result['destination_url']].append(parsed_result)
 		return results
 		
-	def pages(self,target,mode='domain',count=1000,filter_date_newer=None,filter_date_older=None,filter_http_code=None,filter_size_larger=None,filter_size_smaller=None):
+	def pages(self,target,mode='domain',count=1000,timeout=30,filter_date_newer=None,filter_date_older=None,filter_http_code=None,filter_size_larger=None,filter_size_smaller=None):
+		method_name = 'pages'
 		request_url = 'http://ahrefs.com/api.php?AhrefsKey=%s&type=pages&count=%s&mode=%s&target=%s' % (self.key,count,mode,target)
+		print request_url
 		response = self.request(request_url,timeout)
 		results = []
-		for result in response.xpath('//n:result',namespaces={'n': self.namespace}):
-			parsed_result = self.parse_result(result)
+		for result in response.xpath('//n:result',namespaces={'n': self.namespace % method_name}):
+			print result
+			parsed_result = self.parse_result(result,method_name)
+			print parsed_result
 			if not self.filter_date(parsed_result,filter_date_newer,filter_date_older):
 				break
 			if filter_http_code:
-				filter_http_code = [str(code) for code in filter_http_code]
+				filter_http_code = [int(code) for code in filter_http_code]
 				if parsed_result['http_code'] not in filter_http_code:
 					break
 			if filter_size_larger:
