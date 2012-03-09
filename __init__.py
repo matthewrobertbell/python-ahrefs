954faedb77c69abb11a08c27cc2c37fb92810252
Matthew Bell <matthewrobertbell@gmail.com>
2012-03-09 15:50:27 +0000

    moar

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ class ahrefs(object):
 			raise Exception('Mode must be one of: '+', '.join(MODES))
 		kwargs['output'] = 'json'
 		kwargs['AhrefsKey'] = self.key
-		options = '&'.join([k+'='+urllib.quote_plus(str(v)) for k,v in kwargs.items()])
+		options = '&'.join([k+'='+urllib.quote_plus(str(v).strip()) for k,v in kwargs.items()])
 		request_url = 'http://api.ahrefs.com/{method_name}.php?{options}'.format(method_name=method_name, options=options)
 		response = requests.get(request_url)
 		return json.loads(response.content)
@@ -21,5 +21,12 @@ class ahrefs(object):
 	def backlinks(self, target, count=100, mode=MODES[0]):
 		return self.request(method_name='get_backlinks', target=target, count=count, mode=mode)
 
+	def backlinks_count(self, target, mode=MODES[0]):
+		return self.request(method_name='get_backlinks_count', target=target, mode=mode)
+
 	def api_units_left(self):
 		return self.request(method_name='get_units_left')
+
+	def total_count_details(self, target, mode=MODES[0]):
+		return self.request(method_name='get_ref_domains_ips_count', target=target, mode=mode)
+
