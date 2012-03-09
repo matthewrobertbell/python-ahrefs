91274fe771519c4756f604c581f61c02018a2bec
Matthew Bell <matthewrobertbell@gmail.com>
2012-03-09 14:11:21 +0000

    so i heard you like changing your api?

--- __init__.py
+++ __init__.py
@@ -1,5 +1,25 @@
-from ahrefs import *
+import requests
+import json
+import urllib
 
-if __name__ == '__main__':
-	obj = ahrefs('fakekey')
-	print obj.pages('http://www.reddit.com/')
+MODES = ['domain', 'subdomains', 'prefix', 'exact']
+
+class ahrefs(object):
+	def __init__(self,key):
+		self.key = key
+		
+	def request(self, method_name, **kwargs):
+		if kwargs['mode'] not in MODES:
+			raise Exception('Mode must be one of: '+', '.join(MODES))
+		kwargs['output'] = 'json'
+		kwargs['AhrefsKey'] = self.key
+		options = '&'.join([k+'='+urllib.quote_plus(str(v)) for k,v in kwargs.items()])
+		request_url = 'http://api.ahrefs.com/{method_name}.php?{options}'.format(method_name=method_name, options=options)
+		response = requests.get(request_url)
+		return json.loads(response.content)
+
+	def backlinks(self, target, count=100, mode=MODES[0]):
+		return self.request(method_name='get_backlinks', target=target, count=count, mode=mode)
+
+	def api_units_left(self):
+		return self.request(method_name='get_units_left', mode=MODES[0])
