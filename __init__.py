0ca8fcd0f9a8a2b6e43ee61a809974e9a59d66ad
Matthew Bell <matthewrobertbell@gmail.com>
2012-11-15 14:56:38 -0600

    use requests better

--- __init__.py
+++ __init__.py
@@ -1,22 +1,20 @@
 import requests
-import json
 import urllib
+import os
 
 MODES = ['domain', 'subdomains', 'prefix', 'exact']
 
 class ahrefs(object):
-	def __init__(self,key):
-		self.key = key
+	def __init__(self, key=None):
+		self.key = key or os.environ.get('AHREFS_KEY')
 		
 	def request(self, method_name, **kwargs):
 		if 'mode' in kwargs and kwargs['mode'] not in MODES:
 			raise Exception('Mode must be one of: '+', '.join(MODES))
 		kwargs['output'] = 'json'
 		kwargs['AhrefsKey'] = self.key
-		options = '&'.join([k+'='+urllib.quote_plus(str(v).strip()) for k,v in kwargs.items()])
-		request_url = 'http://api.ahrefs.com/{method_name}.php?{options}'.format(method_name=method_name, options=options)
-		response = requests.get(request_url)
-		return json.loads(response.content)
+		request_url = 'http://api.ahrefs.com/{method_name}.php'.format(method_name=method_name)
+		return requests.get(request_url, params=kwargs).json
 
 	def backlinks(self, target, count=100, mode=MODES[0]):
 		return self.request(method_name='get_backlinks', target=target, count=count, mode=mode)
